74145071a3158d038f9e386ff8b173163c0d0fdb
tsclever <tsclever@outlook.com>
2024-08-08 22:11:36 +0800

    组件间数据通信(升级改造)

--- src/Avatar/index.js
+++ src/Avatar/index.js
@@ -1,19 +1,22 @@
 // JS 代码，一般用单引号， JSX 的属性则是用双引号。
-function Avatar({ size }) {
-  const avatar = 'https://cdn.pixabay.com/photo/2023/05/09/01/47/gold-7980065_640.jpg';
-  const descripiton = 'box';
-  const name = '这是一个大宝箱';
+function Avatar({ size = 300, person }) {
+
+  function getImageUrl(imageId){
+    return(
+      'https://cdn.pixabay.com/' +
+      imageId +
+      '.jpg'
+    )
+  }
+
 
   return(
-    <>
-      <h1>{name}</h1>
-      <img 
-        className = "avatar"
-        src = {avatar}
-        alt = {descripiton}
-        width = {size}
-      />
-    </>
+    <img 
+      className = "avatar"
+      src = {getImageUrl(person.imageId)}
+      alt = {person.name}
+      width = {size}
+    />
   )
 }
 
--- src/MyFirstComponent/index.js
+++ src/MyFirstComponent/index.js
@@ -2,9 +2,28 @@ import Avatar from '../Avatar'
 
 function Profile() {
   return (
-    <Avatar 
-      size = {1000}
-    />
+    <>
+      <Avatar 
+        size = {500}
+        person = {{
+            name: '蓝花', 
+            imageId: 'photo/2021/09/13/08/18/blue-flower-6620619_640'
+        }}
+      />
+      <Avatar
+        size={400}
+        person={{
+          name: '天鹅', 
+          imageId: 'photo/2024/01/15/19/40/animal-8510775_640'
+        }}
+      />
+      <Avatar
+        person={{ 
+          name: '兔子',
+          imageId: 'photo/2023/03/12/14/08/bunny-7847028_640'
+        }}
+      />
+    </>
   )
 }
 

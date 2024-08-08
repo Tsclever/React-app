18a11cfe3d3d07c8dc3bd52e766977e8bf55c054
tsclever <tsclever@outlook.com>
2024-08-08 22:35:02 +0800

    父组件向子组件直传JSX(写法更符合HTML)

---
+++ src/Card/index.js
@@ -0,0 +1,10 @@
+function Card({ children }) {
+  return(
+    // <div className="card" style={{width:"500px", height:"500px", background:"green"}}>
+    <div className="card">
+      {children}
+    </div>
+  )
+}
+
+export default Card

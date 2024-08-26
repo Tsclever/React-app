2ffea2ef7633db90c59caecd018cde18ced19188
tsclever <tsclever@outlook.com>
2024-08-26 17:26:35 +0800

    隐藏已经完成项目

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,9 +1,15 @@
-function Item({name, isPacked = false }) {
-  return (
-    <li className="item">
-      { name + (isPacked ? ' ✔' : '') }
-    </li>
-  )
+function Item({name, isPacked = false }) {  
+  if (isPacked) {
+    return null;  
+  }
+  return <li className="item"> {name}</li>;
+
+  // return (
+  //   <li className="item">
+  //     { name + (isPacked ? ' ✔' : '') }
+  //   </li>
+  // )
+
 }
 
 export default Item

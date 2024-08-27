613d55a72dcaedcad317cc593e1567ab3cec85dc
tsclever <tsclever@outlook.com>
2024-08-27 15:33:13 +0800

    更新List组件

---
+++ src/List/data.js
@@ -0,0 +1,31 @@
+export const people = [{
+  id: 0,
+  name: 'AAA',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'BBB',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'CCC',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'DDD',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'EEE',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}];
--- src/List/index.js
+++ src/List/index.js
@@ -1,34 +1,5 @@
-const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-  accomplishment: 'spaceflight calculations',
-  imageId: 'MK3eW3A'
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-  accomplishment: 'discovery of Arctic ozone hole',
-  imageId: 'mynHUSa'
-}, {
-  id: 2,
-  name: 'Mohammad Abdus Salam',
-  profession: 'physicist',
-  accomplishment: 'electromagnetism theory',
-  imageId: 'bE7W1ji'
-}, {
-  id: 3,
-  name: 'Percy Lavon Julian',
-  profession: 'chemist',
-  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
-  imageId: 'IOjWm71'
-}, {
-  id: 4,
-  name: 'Subrahmanyan Chandrasekhar',
-  profession: 'astrophysicist',
-  accomplishment: 'white dwarf star mass calculations',
-  imageId: 'lrWQx8l'
-}];
+import { people } from './data'
+import { getImageUrl } from './utils'
 
 function List() {
   // 使用filter()方法, 过滤数组数据
@@ -38,7 +9,17 @@ function List() {
 
   // 使用map()方法, 渲染页面
   const listItems = chemists.map(person =>
-    <li>{person.id} : {person.name}</li>
+    <li>
+      <img 
+        src = {getImageUrl(person)}
+        alt = {person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        know for {person.accomplishment}
+      </p>
+    </li>
   );
 
   // console.log(listItems);

66c25f1a3baeffa8f1801144ff53774c445955f1
tsclever <tsclever@outlook.com>
2024-08-27 17:06:04 +0800

    用key做页面中元素的唯一标识符

--- src/List/index.js
+++ src/List/index.js
@@ -9,7 +9,7 @@ function List() {
 
   // 使用map()方法, 渲染页面
   const listItems = chemists.map(person =>
-    <li>
+    <li key={person.id}>
       <img 
         src = {getImageUrl(person)}
         alt = {person.name}
@@ -28,3 +28,22 @@ function List() {
 }
 
 export default List;
+
+// 如果没有 <li> 作为根元素包裹所有的内容时需要怎么做呢?
+// 用  <div>  会污染 DOM，可以用 <> </> 来把多个页面元素包装成单个根元素
+// const listItems = people.map(person =>
+//   <>
+//     <h1>{person.name}</h1>
+//     <p>{person.bio}</p>
+//   </>
+// );
+
+// 此时 <> </> 这种简写的形式是无法直接加 key 的，只要先加上 Fragment 的引用：
+// import { Fragment } from 'react';
+
+// const listItems = people.map(person =>
+//   <Fragment key={person.id}>
+//     <h1>{person.name}</h1>
+//     <p>{person.bio}</p>
+//   </Fragment>
+// );

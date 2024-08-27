d613011b9b03effd2211875a8439b19ce5e95f65
tsclever <tsclever@outlook.com>
2024-08-27 15:19:40 +0800

    利用filter()过滤数组数据

--- src/List/index.js
+++ src/List/index.js
@@ -1,17 +1,45 @@
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}];
 
 function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
+  // 使用filter()方法, 过滤数组数据
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
   );
 
-  console.log(listItems)
+  // 使用map()方法, 渲染页面
+  const listItems = chemists.map(person =>
+    <li>{person.id} : {person.name}</li>
+  );
 
   // console.log(listItems);
 
